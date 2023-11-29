266d0442e95b4cb2b75675eaa060517464a2a2ae
Juan Martin <jiwassjuk@frba.utn.edu.com.ar>
2023-11-29 18:35:59 -0300

    ev

--- src/main/java/ar/edu/utn/frba/dds/server/Server.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Server.java
@@ -12,7 +12,12 @@ import ar.edu.utn.frba.dds.server.utils.PrettyProperties;
 
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Server {
   private static Javalin app = null;
@@ -23,11 +28,23 @@ public class Server {
     return app;
   }
 
-  public static void init() throws IOException {
-    Initializer.init();                       //DESCOMENTAR CUANDO ESTE LISTO TOODO
+  public static void init() throws Exception {
+     EntityManagerFactory entityManagerFactory;
+    Initializer.init();
     if(app == null) {
+
+
+      Map<String, String> env = System.getenv();
+
+      entityManagerFactory =  createEntityManagerFactory();
+      String strport = System.getenv("PORT");
+      if (strport == null){
+        strport = "8080";
+      }
+
+
       PrettyProperties.getInstance();
-      Integer port = Integer.parseInt(System.getProperty("port", "8080"));
+      Integer port = Integer.parseInt(System.getenv("PORT"));
       app = Javalin.create(config()).start(port);
       initTemplateEngine();
       AppHandlers.applyHandlers(app);
@@ -37,7 +54,63 @@ public class Server {
         Initializer.init();
       }
     }
+
   }
+
+
+
+  public static EntityManagerFactory createEntityManagerFactory() throws Exception {
+    // https://stackoverflow.com/questions/8836834/read-environment-variables-in-persistence-xml-file
+    Map<String, String> env = System.getenv();
+    Map<String, Object> configOverrides = new HashMap<String, Object>();
+
+    String[] keys = new String[] {
+        "DATABASE_URL",
+        "javax__persistence__jdbc__driver",
+        "javax__persistence__jdbc__password",
+        "javax__persistence__jdbc__url",
+        "javax__persistence__jdbc__user",
+        "hibernate__hbm2ddl__auto",
+        "hibernate__connection__pool_size",
+        "hibernate__show_sql" };
+
+    for (String key : keys) {
+
+      try{
+        if (key.equals("DATABASE_URL")) {
+
+          // https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-java
+          String value = env.get(key);
+          URI dbUri = new URI(value);
+          String username = dbUri.getUserInfo().split(":")[0];
+          String password = dbUri.getUserInfo().split(":")[1];
+          //javax.persistence.jdbc.url=jdbc:postgresql://localhost/dblibros
+          value = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();// + "?sslmode=require";
+          configOverrides.put("javax.persistence.jdbc.url", value);
+          configOverrides.put("javax.persistence.jdbc.user", username);
+          configOverrides.put("javax.persistence.jdbc.password", password);
+          configOverrides.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+
+          //  configOverrides.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        }
+        // no se pueden poner variables de entorno con "." en la key
+        String key2 = key.replace("__",".");
+        if (env.containsKey(key)) {
+          String value = env.get(key);
+          configOverrides.put(key2, value);
+        }
+      } catch(Exception ex){
+        System.out.println("Error configurando " + key);
+      }
+    }
+    System.out.println("Config overrides ----------------------");
+    for (String key : configOverrides.keySet()) {
+      System.out.println(key + ": " + configOverrides.get(key));
+    }
+    return Persistence.createEntityManagerFactory("db", configOverrides);
+  }
+
+
 
   private static Consumer<JavalinConfig> config() {
     return config -> {
