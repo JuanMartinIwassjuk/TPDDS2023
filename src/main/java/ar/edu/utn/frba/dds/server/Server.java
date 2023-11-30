8cea3c145e2fd9a41b4f2576cb55485be863b491
Juan Martin <jiwassjuk@frba.utn.edu.com.ar>
2023-11-29 21:13:18 -0300

    a

--- src/main/java/ar/edu/utn/frba/dds/server/Server.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Server.java
@@ -66,49 +66,14 @@ public class Server {
     Map<String, String> env = System.getenv();
     Map<String, Object> configOverrides = new HashMap<String, Object>();
 
-    String[] keys = new String[] {
-        "DATABASE_URL",
-        "javax__persistence__jdbc__driver",
-        "javax__persistence__jdbc__password",
-        "javax__persistence__jdbc__url",
-        "javax__persistence__jdbc__user",
-        "hibernate__hbm2ddl__auto",
-        "hibernate__connection__pool_size",
-        "hibernate__show_sql" };
 
-    for (String key : keys) {
+    configOverrides.put("DATABASE_URL",env.get("DATABASE_URL"));
+    configOverrides.put("javax.persistence.jdbc.url",  env.get("javax.persistence.jdbc.url"));
+    configOverrides.put("javax.persistence.jdbc.user", env.get("javax.persistence.jdbc.user"));
+    configOverrides.put("javax.persistence.jdbc.password", env.get("javax.persistence.jdbc.password"));
+    configOverrides.put("javax.persistence.jdbc.driver", env.get("javax.persistence.jdbc.driver"));
+    configOverrides.put("hibernate.dialect", env.get("hibernate.dialect"));
 
-      try{
-        if (key.equals("DATABASE_URL")) {
-
-          // https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-java
-          String value = env.get(key);
-          URI dbUri = new URI(value);
-          String username = dbUri.getUserInfo().split(":")[0];
-          String password = dbUri.getUserInfo().split(":")[1];
-          //javax.persistence.jdbc.url=jdbc:postgresql://localhost/dblibros
-          value = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();// + "?sslmode=require";
-          configOverrides.put("javax.persistence.jdbc.url", value);
-          configOverrides.put("javax.persistence.jdbc.user", username);
-          configOverrides.put("javax.persistence.jdbc.password", password);
-          configOverrides.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-
-          configOverrides.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        }
-        // no se pueden poner variables de entorno con "." en la key
-        String key2 = key.replace("__",".");
-        if (env.containsKey(key)) {
-          String value = env.get(key);
-          configOverrides.put(key2, value);
-        }
-      } catch(Exception ex){
-        System.out.println("Error configurando " + key);
-      }
-    }
-    System.out.println("Config overrides ----------------------");
-    for (String key : configOverrides.keySet()) {
-      System.out.println(key + ": " + configOverrides.get(key));
-    }
     return Persistence.createEntityManagerFactory("db", configOverrides);
   }
 
